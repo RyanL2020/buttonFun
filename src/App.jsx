f81808cec8dabf3501a791e56f78f73ba08778bd
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-08-25 16:48:50 +0000

    button with function

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import './App.css'
 import Button from './Button'
 
 export default function App() {
-    const [count, setCount] = React.useState(0)
+ 
+    const [count, setCount] = React.useState(0);
   const handleClick = () => {
-    setCount(count + 1)
-  } 
+    setCount(count + 1);
+  }; 
   return (
   
     <div>
-      <button onlick={handleClick} >
+      <button  onClick={handleClick}>
         Click ME !!!!
       </button>
       {count}
     </div>
-  )
+  );
   
 }
